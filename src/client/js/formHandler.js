4b09e9008bf7664b1bf4c5899354208c2cfd048d
Mario Hammer <mario.hammer@logicline.de>
2020-02-27 18:51:03 +0100

    added addition of result

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -9,7 +9,32 @@ function handleSubmit(event) {
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
-        //document.getElementById('results').innerHTML = res.polarity
+        const results = document.getElementById('results');
+        
+        const resultbox = document.createElement('div');
+        const title = document.createElement('div');
+        const polarity = document.createElement('div');
+        const subjectivity = document.createElement('div');
+        const text = document.createElement('div');
+
+        resultbox.classList.add('resultbox');
+        title.classList.add('title');
+        polarity.classList.add('polarity');
+        polarity.classList.add(res.polarity == 'positive' ? 'pos' : 'neg');
+        subjectivity.classList.add('subjectivity');
+        text.classList.add('text');
+
+        title.textContent = url;
+        polarity.textContent = res.polarity;
+        subjectivity.textContent = res.subjectivity;
+        text.textContent = res.text.substring(0, 1000);
+
+        resultbox.appendChild(title);
+        resultbox.appendChild(polarity);
+        resultbox.appendChild(subjectivity);
+        resultbox.appendChild(text);
+
+        results.appendChild(resultbox);
     });
 }
 
